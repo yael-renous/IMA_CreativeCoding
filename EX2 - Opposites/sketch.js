f95da21572bdfece38c5170c6f9d20594fc5b17c
Yael Renous <yaelrenous@gmail.com>
2024-07-21 21:57:21 -0400

    metlting works

--- EX2 - Opposites/sketch.js
+++ EX2 - Opposites/sketch.js
@@ -1,38 +1,38 @@
 
 let matrix = [];
-
+let bgColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);//size(600, 400);
   angleMode(DEGREES);
-  colorMode(HSB);
-  stroke(199, 80, 88);
+  colorMode(RGB);
+  let colorColor = color(random(255), random(255), random(255));
+  stroke(colorColor);
   strokeWeight(0.8);
   noFill();
+  bgColor='black';
   createMatrix();
   pixelDensity(1);
 }
 
 function draw() {
   clear();
- // orbitControl(4, 4);//Mouse control
-
+  background(bgColor);
   for (let s of matrix) {
-    // cube.rotate();
-    s.rotate();
+    s.update();
     s.display();
   }
 }
 
 function createMatrix() {
-  var sphereRadius = width/5;
-  var padding = sphereRadius/2;
+  var sphereRadius = width / 5;
+  var padding = sphereRadius / 2;
 
   for (let x = 0; x < width; x = x + sphereRadius + padding) {
-    for (let y = 0; y < height; y = y +sphereRadius+ padding) {
+    for (let y = 0; y < height; y = y + sphereRadius + padding) {
       let s = new BumpySphere(
-        x - width / 2 + sphereRadius ,
-        y - height / 2 + sphereRadius ,
+        x - width / 2 + sphereRadius,
+        y - height / 2 + sphereRadius,
         0,
         sphereRadius
       );
@@ -41,7 +41,7 @@ function createMatrix() {
   }
 }
 
-function mousePressed(){
+function mousePressed() {
   for (let s of matrix) {
     s.toggleMelt();
   }
